ac21dff2f1616857255ef362e1e56247402decf0
H. Emre Aral <78971902+emrearal@users.noreply.github.com>
2021-03-14 11:31:13 +0300

    Update anaekran.java

--- anaekran.java
+++ anaekran.java
@@ -150,11 +150,11 @@ public class anaekran implements ActionListener {
        menubar.add(firmalar);
        menubar.add(raporlar);
        menubar.add(araclar);
-       menubar.add(yedek);   
+       //menubar.add(yedek);   
        menubar.add(yardim);
      
        firmalar.add(firmahesaplari);
-       yedek.add(yedekal);
+       //yedek.add(yedekal);  uzak sql sunucuda çalismaz.
        //yedek.add(geriyukle); şimdilik yedek geri yüklemeyi kullanmıyoruz.
        araclar.add(notdefteri);
        araclar.add(hesapmakinesi);
